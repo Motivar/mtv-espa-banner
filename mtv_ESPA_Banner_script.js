8b8e91662d07aebe147ebf4ffeacd06764c2d6ae
gnnpls <n.giannopoulos@motivar.io>
2022-02-24 10:47:15 +0200

    fixes

--- mtv_ESPA_Banner_script.js
+++ mtv_ESPA_Banner_script.js
@@ -2,7 +2,7 @@ function mtv_espa_banner(options) {
     var defaults = {
         position: 'bottom',
         padding: '5px',
-        height: '50px',
+        height: '60px',
         img_max_width: '320px',
         img_url: '',
         z_index: 999999999,
@@ -30,13 +30,14 @@ function mtv_espa_banner(options) {
 }
 
 function mtv_espa_print_css(Options) {
-    var position_style = 'bottom:0px;'
+    var position_style = 'bottom:0px;';
     if (Options.position == 'top') {
         position_style = 'top:0px;';
     }
 
     var styles = `
     #mtv-espa-wrapper {
+        box-sizing:content-box;
     width: 100%;
     padding: ${Options.padding};
     position: fixed;
@@ -45,7 +46,6 @@ function mtv_espa_print_css(Options) {
     right:0px;
     z-index:  ${Options.z_index};
     background: white;
-    border-top: solid 2px;
     height: ${Options.height};
     }
     #mtv-espa-wrapper .mtv-espa-image {
@@ -56,8 +56,9 @@ function mtv_espa_print_css(Options) {
     }
      #mtv-espa-wrapper .mtv-espa-image   img {
             width: 100%;
-            min-width:100px;
-            height:100%;
+            min-width:50px;
+            max-heigth:${Options.height};
+            height:100%:
     }
     
 `.trim();
